ff7d31b3eb3650aed0a68a4c0a69c7cd225dd81a
Janessa Garrow <janessa.garrow@gmail.com>
2020-01-23 15:30:31 -0700

    Sort blog posts by date to fix pagination. Add Link back to blog home page

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,7 +9,9 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 
     const result = await graphql(`
         {
-            allMarkdownRemark {
+            allMarkdownRemark(
+                sort: { fields: [frontmatter___date], order: DESC }
+            ) {
                 edges {
                     node {
                         id
